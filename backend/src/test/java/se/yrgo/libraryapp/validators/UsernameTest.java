8e7b7098305eef7f92ac8e26cbd760fc4d12654c
MarioGit24 <marbug0222@skola.goteborg.se>
2025-11-06 11:06:06 +0100

    (Uppgift 1 & 2) - Adding dependencies to pom and testing in UsernameTest

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,17 +1,28 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UsernameTest {
-    @Test
-    void correctUsername() {
-        assertTrue(Username.validate("bosse"));
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Mario", "MArsiio", "Ma-:A-Z"})
+    @EmptySource
+    void correctUsername(String name1) {
+        boolean result = Username.validate(name1);
+        assertThat(result).isTrue();
     }
 
-    @Test
-    void incorrectUsername() {
-        assertFalse(Username.validate("name with space"));
+    @ParameterizedTest
+    @ValueSource(strings = {"Mario", "MArio1", "AmArios2"})
+    @EmptySource
+    void incorrectUsername(String name) {
+        boolean result = Username.validate(name);
+        assertThat(result).isFalse();
     }
 }
