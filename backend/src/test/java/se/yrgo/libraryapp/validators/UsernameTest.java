0c8b1d4dc19a9a9e80a6e7bb8855b4cdb7e1d7f0
MarioGit24 <marbug0222@skola.goteborg.se>
2025-11-18 18:32:25 +0100

    fix(uppgift-2) - Removed faulty testing params

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,25 +1,23 @@
 package se.yrgo.libraryapp.validators;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class UsernameTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"Mario", "MArsiio", "Ma-:A-Z"})
-    @EmptySource
+    @ValueSource(strings = {"Mario", "MArsiio", "Ma_A-Z"})
     void correctUsername(String name1) {
         boolean result = Username.validate(name1);
         assertThat(result).isTrue();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Mario", "MArio1", "AmArios2"})
+    @ValueSource(strings = {"Mar", "MArio 1", "Mrios2_", "Mario!!!"})
     @EmptySource
     void incorrectUsername(String name) {
         boolean result = Username.validate(name);
