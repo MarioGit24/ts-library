34f611f99b1b45dfa861a8d2fadb56bd8100ec69
Mario B <marbug0222@skola.goteborg.se>
2025-11-29 15:48:21 +0100

    (Klar)Uppgift-4

--- backend/src/test/java/se/yrgo/libraryapp/validators/RealNameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/RealNameTest.java
@@ -1,23 +1,54 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RealNameTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {
-            "användare testord",
-            "svordom användare",
-            "hemligt namn",
-            "ett förbjudet testord"
-    })
+    @CsvFileSource(resources = "/bad_words.txt")
     void shouldBlockWordsFromFile(String name) {
         boolean result = RealName.validate(name);
         assertThat(result).isFalse();
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/bad_words.txt")
+    void blockWordsFromFileWrittenInLeet(String name) {
+        boolean result = RealName.validate(name);
+        assertThat(result).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Mario",
+            "MARIO",
+            "mario",
+            "MaRiO",
+            "MArio_123-2",
+            "MARIO-_123_2",
+            "mario-_222",
+            "Mario/_-:23"
+    })
+    void shouldAllowValidNames(String name) {
+        boolean result = RealName.validate(name);
+        assertThat(result).isTrue();
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
