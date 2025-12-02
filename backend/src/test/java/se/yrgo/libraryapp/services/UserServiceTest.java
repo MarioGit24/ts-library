311fd59798d961217cf0157946681105cbf47f97
Mario B <marbug0222@skola.goteborg.se>
2025-12-02 20:06:09 +0100

    (Uppgift-1): creating test for UserService

--- backend/src/test/java/se/yrgo/libraryapp/services/UserServiceTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/services/UserServiceTest.java
@@ -1,9 +1,14 @@
 package se.yrgo.libraryapp.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import se.yrgo.libraryapp.dao.UserDao;
 import se.yrgo.libraryapp.entities.LoginInfo;
@@ -12,12 +17,23 @@ import se.yrgo.libraryapp.entities.UserId;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.STRICT_STUBS)
 public class UserServiceTest {
     @Mock
     private UserDao userDao;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private PasswordEncoder encoder;
+
     @Test
     @SuppressWarnings("deprecation")
     void correctLogin() {
@@ -34,6 +50,55 @@ public class UserServiceTest {
         assertThat(userService.validate(username,
                 password)).isEqualTo(Optional.of(id));
     }
+
+    @Test
+    void registerUser() {
+        String name = "testuser";
+        String realName = "Ian 0'Toole";
+
+        when(userDao.register(eq(name), anyString(), eq("Ian 0\\'Toole")))
+                .thenReturn(true);
+
+        boolean result = userService.registerUser(name, realName, "hello");
+
+        assertThat(result).isTrue();
+
+    }
+
+
+
+    @Test
+    void registerUserFail() {
+        String name = "testuser";
+        String realName = "Ian 0_Toole";
+
+        when(userDao.register(eq(name), anyString(), anyString()))
+                .thenReturn(false);
+
+        boolean result = userService.registerUser(name, realName, "hello");
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void failToCallDao() {
+        String name = "testuser";
+        String realName = "Ian 0'Toole";
+
+        String expectedCleanName = "Ian 0\\'Toole";
+
+        when(userDao.register(eq(name), anyString(), eq(expectedCleanName)))
+                .thenReturn(false);
+
+        boolean result = userService.registerUser(name, realName, "hello");
+
+        assertThat(result).isFalse();
+
+        verify(userDao).register(eq(name), anyString(), eq(expectedCleanName));
+    }
+
+
+
 
 
 }
